ac4715b9020187c1c8fe2537de9be4ed43fb9bbc
SnigdhaDev <110219996+SnigdhaDev@users.noreply.github.com>
2022-10-21 12:20:27 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,9 @@
+const readline = require("readline-sync");
+//const prompt= require("prompt-sync")();
+console.log("Hello!Welcome to Node.js");
+a=10;
+b=20;
+console.log("%d+%d=%d",a,b,(a+b));
+//const num=prompt("Enter a number:");
+let n = Number(readline.question("Enter n value:"));
+console.log("n value is:%d",num);
